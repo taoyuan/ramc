08b57e3d90e895dce4e7263174c8f86b2d3fe7ea
Yuan Tao <towyuan@outlook.com>
2016-09-01 12:13:58 +0800

    default accept 'application/json' for method returns

--- examples/parser-swagger-v2/parse.js
+++ examples/parser-swagger-v2/parse.js
@@ -3,12 +3,10 @@
 var fs = require('fs');
 var SwaggerV2 = require('../../lib/parsers').SwaggerV2;
 
-var file = 'uber.json';
+var file = '../apis/uber.json';
 var swagger = JSON.parse(fs.readFileSync(file, 'UTF-8'));
 var data = SwaggerV2.parse({
-  moduleName: 'Dolink',
-  host: 'api-beta.dolink.io',
-  swagger: swagger
+  spec: swagger
 });
 
 console.log(JSON.stringify(data, null, '  '));
